61e291cefb4c6d472af278a3bff6644245d54ce2
madureira <marlos.madureira@gmail.com>
2025-08-24 13:24:28 -0300

    Executa em Paralelo

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from pyMigrando_tb_whatszap_call_log import *
 from pyMigrando_tb_whatszap_index_zapcontatos_new import *
 from pyBibliotecas import *
+import threading
+
+def run_parallel():
+    print_color("\nInicializando....\n", 32)
+
+    # Cria as threads para rodar as funções
+    t1 = threading.Thread(target=mainNewLogs, name="Thread-NewLogs")
+    t2 = threading.Thread(target=mainCallLogs, name="Thread-CallLogs")
+
+    # Inicia as threads
+    t1.start()
+    t2.start()
+
+    # Aguarda ambas terminarem
+    t1.join()
+    t2.join()
 
 if __name__ == '__main__':
 
     print_color(f"\nInicializando....\n", 32)
 
-    mainNewLogs()
-
-    mainCallLogs()
+    run_parallel()
